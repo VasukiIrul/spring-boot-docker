3d648c6af6919b4b26f1bdea126ddc6c40386c06
vasuki <vasuki.tapfer@gmail.com>
2022-12-05 08:10:17 +0530

    deleted Docker

--- src/main/java/com/Demo/WelcomeController.java
+++ src/main/java/com/Demo/WelcomeController.java
@@ -2,15 +2,25 @@ package com.Demo;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
 public class WelcomeController {
 
+	@PostMapping("/welcome")
 	public ResponseEntity<String> welcome()
 	{
 		return new ResponseEntity<String> ("welcome to my application",HttpStatus.OK);
+	}
+	
+	@PostMapping("/add")
+	public ResponseEntity<Integer> addTwoValues(@RequestParam("a") int val1,@RequestParam("b") int val2)
+	{
+		int result=val1+val2;
+		return new ResponseEntity<Integer>(result,HttpStatus.OK);
 	}
 }
--- src/test/java/com/Demo/DockerSpringBootDemoApplicationTests.java
+++ src/test/java/com/Demo/DockerSpringBootDemoApplicationTests.java
@@ -1,13 +1,21 @@
 package com.Demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
 class DockerSpringBootDemoApplicationTests {
 
+	@Autowired
+	WelcomeController controller;
+
 	@Test
-	void contextLoads() {
+	public void addTest() {
+		ResponseEntity<Integer> result = controller.addTwoValues(20, 12);
+		Assertions.assertEquals(result.getBody(), 32);
 	}
 
 }
